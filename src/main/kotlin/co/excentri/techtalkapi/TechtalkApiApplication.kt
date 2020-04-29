1a165fed5bd7447da3c9345f198586f723e83ac0
Vinícius Savi Silva <vinicius.silva@sofist.com.br>
2020-04-29 12:47:54 -0300

    [feat] Base project.

---
+++ src/main/kotlin/co/excentri/techtalkapi/TechtalkApiApplication.kt
@@ -0,0 +1,11 @@
+package co.excentri.techtalkapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TechtalkApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<TechtalkApiApplication>(*args)
+}
---
+++ src/test/kotlin/co/excentri/techtalkapi/TechtalkApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package co.excentri.techtalkapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TechtalkApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
